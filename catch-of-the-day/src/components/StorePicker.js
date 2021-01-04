814831aa378f01c7a2042dc83c1a88b1f91a483d
Schatfield <dgoldmine@icloud.com>
2021-01-04 15:36:45 -0600

    re-added the components, imports, etc, trying pull request again

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from 'react'; //import you ingredients
+
+
+//make the sausage
+class StorePicker extends React.Component {
+    render() {
+        return <p>hello!</p>
+    } 
+} 
+
+
+//surface the sausage to be available to the people (or your files)
+export default StorePicker; 
