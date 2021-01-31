554b292e84b70d360893b4b98894c6e67569ef4f
Schatfield <dgoldmine@icloud.com>
2021-01-31 15:42:41 -0600

    building out of Header component

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="top">
+                <h1>Catch of the Day</h1>
+            </header>
+        )
+    }
+}
