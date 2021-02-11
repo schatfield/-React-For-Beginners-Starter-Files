53d32103b15a814f3ba356458bae7154a5dbf315
Schatfield <dgoldmine@icloud.com>
2021-02-11 17:13:37 -0600

    resolving conflicts

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,18 +1,5 @@
 import React from 'react'; //import you ingredients
 
-<<<<<<< HEAD
-
-//make the sausage
-class StorePicker extends React.Component {
-    render () {
-        return <p>hello!</p>
-    } 
-} 
-
-
-//surface the sausage to be available to the people (or your files)
-export default StorePicker; 
-=======
 //make the sausage
 class StorePicker extends React.Component {
     render() {
@@ -22,4 +9,3 @@ class StorePicker extends React.Component {
 
 //surface the sausage to be available to the people (or your files)
 export default StorePicker;
->>>>>>> setUp
